34fe4a00fbc7ea9f9bf1ade47474196f7db7483a
Stephen William <stevedelta99@yahoo.com>
2021-06-08 21:01:01 +0700

    Fix MainActivity onBack to show Dialog

--- app/src/main/java/stephenwk/com/soa_guildwars2/app/MainActivity.kt
+++ app/src/main/java/stephenwk/com/soa_guildwars2/app/MainActivity.kt
@@ -1,19 +1,26 @@
 package stephenwk.com.soa_guildwars2.app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import stephenwk.com.soa_guildwars2.R
+import stephenwk.com.soa_guildwars2.app.apikey.ApiKeyActivity
 import stephenwk.com.soa_guildwars2.app.character.CharacterFragment
 import stephenwk.com.soa_guildwars2.app.guild.GuildFragment
 import stephenwk.com.soa_guildwars2.app.home.HomeFragment
+import stephenwk.com.soa_guildwars2.app.home.HomeViewModel
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var fragment: Fragment
+
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,5 +46,20 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+    override fun onBackPressed() {
+        MaterialAlertDialogBuilder(this@MainActivity)
+            .setTitle(resources.getString(R.string.logout))
+            .setMessage(resources.getString(R.string.logout_desc))
+            .setNegativeButton(resources.getString(R.string.stay)) { dialog, _ ->
+                dialog.dismiss()
+            }
+            .setPositiveButton(resources.getString(R.string.logout)) { _, _ ->
+                viewModel.removeApiKey()
+                val intent = Intent(this@MainActivity, ApiKeyActivity::class.java)
+                this@MainActivity.startActivity(intent)
+            }
+            .show()
     }
 }
